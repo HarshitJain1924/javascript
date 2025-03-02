5ff21dcd195bc564228fceb4db46fb1e434021ec
Harshit <harshitjain1924@gmail.com>
2025-03-03 04:44:58 +0530

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14442342;
+let accountEmail = "hitesh@gooogle.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "hj@hj.com"
+accountPassword = "2314123"
+accountCity = "Bengaluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
