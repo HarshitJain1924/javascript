6277b86b04661e3ab830412c90dbe21b1585049f
Harshit <harshitjain1924@gmail.com>
2025-03-03 05:27:34 +0530

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "33";
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
 
 
 // "33" => 33
@@ -13,7 +13,7 @@ console.log(typeof valueInNumber);
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 
 // 1 => true; 0 => false
@@ -22,4 +22,28 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber);
-console.log(stringNumber);
+//console.log(stringNumber);
+
+//************************************OPERATIONS****************************************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Harshit"
+
+let str3 = str1+str2;
+//console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"3");
